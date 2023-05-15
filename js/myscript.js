fc4e358220870064521fedfc55d1cc868c5c2e30
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-15 22:33:44 +0200

    Aggiunto isNan ma c'è un errore

--- js/myscript.js
+++ js/myscript.js
@@ -10,8 +10,15 @@ button.addEventListener("click",
     function(){
         let price =inputKm.value * 0.233
         const outputPrice = document.querySelector('p');
+        console.log(typeof(inputAge.value))
+        console.log(typeof(inputKm.value))
 
-        if (inputAge.value < 18){
+
+        if(isNaN(inputAge.value) || isNaN(inputKm.value)){
+            price="Qualcosa è andato storto, ricontrolla i dati";
+            inputAge.classList.add("bg-danger");
+            inputAge.classList.add("bg-danger");
+        }else if (inputAge.value < 18){
             price= price - (( price / 100) * 19.4)
             console.log(price + " minorenne")
         }else if(inputAge.value > 65){
@@ -21,13 +28,15 @@ button.addEventListener("click",
             console.log(price + " normale")
         }
 
+        console.log(typeof(inputAge.value))
+        console.log(typeof(inputKm.value))
+
+
         outputPrice.innerHTML = "€" +  price.toFixed(2);
-        document.getElementById("bis-username").innerHTML = " Nominativo passeggero : " + inputName.value
-        document.getElementById("bis-km").innerHTML = "Il biglietto è per : " + inputKm.value+" km"
-        document.getElementById("bis-age").innerHTML =  " L' età del passereo è :" + inputAge.value
-        document.getElementById("bis-mail").innerHTML =  "L' email inserita è : " + inputMail.value
-
-
+        document.getElementById("bis-username").innerHTML = " Nominativo passeggero : " + inputName.value;
+        document.getElementById("bis-km").innerHTML = "Il biglietto è per : " + inputKm.value+" km";
+        document.getElementById("bis-age").innerHTML =  " L' età del passereo è :" + inputAge.value + "anni";
+        document.getElementById("bis-mail").innerHTML =  "L' email inserita è : " + inputMail.value;
     }
 );
 
