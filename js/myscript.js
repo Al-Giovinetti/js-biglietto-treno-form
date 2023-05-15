571acefc33a884f511ec7b71dfcf3aee47589fc0
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-15 15:43:56 +0200

    Bottone collegato all' evento con prova

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,9 @@ const inputKm = document.getElementById("user-km");
 const inputAge = document.getElementById("user-age");
 
 const button = document.querySelector('button');
+
+button.addEventListener("click",
+    function(){
+        alert("tanto per");
+    }
+);
