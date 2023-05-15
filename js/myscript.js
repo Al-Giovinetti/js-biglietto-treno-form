66dac586f5aa0cd6daa47b4b7d11fc285120fb0e
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-15 17:36:21 +0200

    Risultato stampato in console

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,28 @@
-const inputKm = document.getElementById("user-km");
-const inputAge = document.getElementById("user-age");
+const inputKm =document.getElementById("user-km");
+const inputAge =document.getElementById("user-age");
 
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+
+console.log(inputKm)
+console.log(inputAge)
 
 button.addEventListener("click",
     function(){
-        alert("tanto per");
+        let price =inputKm.value * 0.233
+
+        if (inputAge.value < 18){
+            price=price - (( price / 100) * 19.4)
+            console.log(price)
+            console.log("minorenne")
+        }else if(inputAge.value > 65){
+            price=price - (( price / 100) * 37.7)
+            console.log(price)
+            console.log("over 65")
+        }else{
+            console.log(price)
+            console.log("normale")
+            
+        }
     }
+
 );
