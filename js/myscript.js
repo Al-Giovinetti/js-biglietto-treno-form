4de45068cfb3c283195b5f4533f1d2d40d30751a
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-15 21:45:00 +0200

    Lay out pagina

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,34 @@
-const inputKm =document.getElementById("user-km");
-const inputAge =document.getElementById("user-age");
+const inputKm = document.getElementById("user-km");
+const inputAge = document.getElementById("user-age");
+
+const inputName = document.getElementById("user-name")
+const inputMail = document.getElementById("user-mail")
 
 const button = document.querySelector("button");
-
-console.log(inputKm)
-console.log(inputAge)
 
 button.addEventListener("click",
     function(){
         let price =inputKm.value * 0.233
+        const outputPrice = document.querySelector('p');
 
         if (inputAge.value < 18){
-            price=price - (( price / 100) * 19.4)
-            console.log(price)
-            console.log("minorenne")
+            price= price - (( price / 100) * 19.4)
+            console.log(price + " minorenne")
         }else if(inputAge.value > 65){
-            price=price - (( price / 100) * 37.7)
-            console.log(price)
-            console.log("over 65")
+            price= price - (( price / 100) * 37.7)
+            console.log(price + " over 65")
         }else{
-            console.log(price)
-            console.log("normale")
-            
+            console.log(price + " normale")
         }
-    }
 
+        outputPrice.innerHTML = "€" +  price.toFixed(2);
+        document.getElementById("bis-username").innerHTML = " Nominativo passeggero : " + inputName.value
+        document.getElementById("bis-km").innerHTML = "Il biglietto è per : " + inputKm.value+" km"
+        document.getElementById("bis-age").innerHTML =  " L' età del passereo è :" + inputAge.value
+        document.getElementById("bis-mail").innerHTML =  "L' email inserita è : " + inputMail.value
+
+
+    }
 );
+
+
