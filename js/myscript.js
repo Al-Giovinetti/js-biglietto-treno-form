bd8505d444f9f64d5907d892b42e5a17d94e5f52
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-27 17:36:15 +0200

    Aggiornata 2

--- js/myscript.js
+++ js/myscript.js
@@ -5,39 +5,47 @@ const inputName = document.getElementById("user-name");
 const inputMail = document.getElementById("user-mail");
 
 const button = document.querySelector("button");
+const checkboxs = document.querySelector("input.form-check-input")
 
-button.addEventListener("click",
-    function(){
-        let price = parseInt(inputKm.value * 0.233)
-        const outputPrice = document.querySelector('p');
+button.addEventListener("click",function(){
+
+    let price = parseInt(inputKm.value * 0.233)
+    const outputPrice = document.querySelector('p');
         
-        console.log(typeof(inputAge.value))
-        console.log(typeof(inputKm.value))
+    //console.log(typeof(inputAge.value))
+    //console.log(typeof(inputKm.value))
 
 
-        if(isNaN(parseInt(inputAge.value)) || isNaN(parseInt(inputKm.value))){
-            inputAge.classList.add("bg-danger");
-            inputKm.classList.add("bg-danger");
-            price="Qualcosa è andato storto, ricontrolla i dati";
-        }else if (inputAge.value < 18){
-            price= price - (( price / 100) * 19.4);
-            console.log(price + " minorenne");
-        }else if(inputAge.value > 65){
-            price= price - (( price / 100) * 37.7);
-            console.log(price + " over 65");
-        }else{
-            console.log(price + " normale");
-        }
-
-        console.log(typeof(inputAge.value));
-        console.log(typeof(inputKm.value));
 
 
-        outputPrice.innerHTML = "€" +  price.toFixed(2);
-        document.getElementById("bis-username").innerHTML = " Nominativo passeggero : " + inputName.value;
-        document.getElementById("bis-km").innerHTML = "Il biglietto è per : " + inputKm.value+" km";
-        document.getElementById("bis-age").innerHTML =  " L' età del passereo è :" + inputAge.value + "anni";
-        document.getElementById("bis-mail").innerHTML =  "L' email inserita è : " + inputMail.value;
+    if(isNaN(parseInt(inputAge.value)) || isNaN(parseInt(inputKm.value))){
+        inputAge.classList.add("bg-danger");
+        inputKm.classList.add("bg-danger");
+        price="Qualcosa è andato storto, ricontrolla i dati";
+    }else if (inputAge.value < 18){    
+        price= price - (( price / 100) * 19.4);
+        console.log(price + " minorenne");
+    }else if(inputAge.value > 65){
+        price= price - (( price / 100) * 37.7);
+        //console.log(price + " over 65");
+    }else{
+        //console.log(price + " normale");
+    }
+
+    //console.log(typeof(inputAge.value));
+    //console.log(typeof(inputKm.value));
+
+
+    outputPrice.innerHTML = "€" +  price.toFixed(2);
+    document.getElementById("bis-username").innerHTML = " Nominativo passeggero : " + inputName.value;
+    document.getElementById("bis-km").innerHTML = "Il biglietto è per : " + inputKm.value+" km";
+    document.getElementById("bis-age").innerHTML =  " L' età del passereo è :" + inputAge.value + "anni";
+    document.getElementById("bis-mail").innerHTML =  "L' email inserita è : " + inputMail.value;
+
+    inputName.value=" "
+    inputKm.value=" "
+    inputAge.value=" "
+    inputMail.value=" "
     }
 );
 
